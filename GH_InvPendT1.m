e50b0daeb030e71fbfa49d4044e307755be8dc68
Sayed <sayed.thangal@locomotionlab.com>
2023-05-19 19:20:21 -0700

    First commit

---
+++ GH_InvPendT1.m
@@ -0,0 +1,225 @@
+function [t,y,Tswitch]=GH_InvPendT1(B,L,M,yi)
+% Function to find the optimum time to switch direction of bang bang torque
+% simulate the results and output inertial delay
+global g
+graph=0;
+%% Test case : Comment out function definition above and uncomment below section. Aso comment out the "end" at line 165
+%{
+clear all;close all;clc;
+graph=1;
+g=-9.8066;
+% Scaling values based on Mass
+%M=0.005;XX=1000;Tunit='(ms)';% for 5 gram
+%M=1;XX=1000;Tunit='(ms)';% for 1 kg
+%M=5000;XX=1;Tunit='(s)';% for 5 gram
+M=10000;XX=1;Tunit='(s)';
+
+% Limb inertial properties from fitlm Type 1 regression, all final values in SI units
+L_HlimbA=0.163;L_HlimbB=0.357;% Mammalian fore limb
+L_Hlimb=L_HlimbA*M.^L_HlimbB;
+L_FlimbA=0.161;L_FlimbB=0.384;% Mammalian fore limb
+L_Flimb=L_FlimbA*M.^L_FlimbB;
+%Llimb=L_Hlimb;% old method
+L=(L_Hlimb+L_Flimb)./2;
+% Muscle scaling. Alexander et. al.  (1981), Table 1 All non-hoppers, ankle extensors
+% Assuming muscle force (100 N)=muscle mass (kg)/ fiber length (m)
+Mmusc=(5.1*M.^0.97)/1000;% mass of muscle (kg), using ankle extensors
+FLmusc=(10.6*M.^0.14)/1000;% fiber length muscle (m)
+rho=1060;%density of muscle in kg/m^3 (Mendez and keys 1960)
+CSmusc=Mmusc./rho./FLmusc.*(100^2);% Cross sectional area of muscle in cm^2
+Fmusc=CSmusc.*20;% assuming 20N/cm^2 muscle isometric stress. varies from 20-30 N/cm^2
+% REfs: Allometry of muscle,tendon and elastic energy storage capacity in
+% mammals Shadwick and Pollock 1994
+% Close R 1972 Dynamic Properties of mammalian skeletal muscles 
+Amusc=(9.4*M.^0.38)/1000;% Moment arm value from Table II ankle extensors-all non hoppers
+B=4*Fmusc.*Amusc;% Torque by the 4 leg muscles (Nm)
+
+%mode=0;Iangle=-5;modnam='Angle Perturbation';% set mode to 0 for initial position/0 velocity  
+mode=1;Fr=-0.21;modnam='Froude Perturbation';%set mode to 1 for initial velocity/0 position
+%mode=1;Fr=-0.49;modnam='Froude Perturbation';%set mode to 1 for initial velocity/0 position
+
+%Fr=-sqrt(2)/3;
+%Fr=-0.5199359;
+
+if mode==0 % sim recovery from angle perturbation
+    yi(1) = deg2rad(Iangle);
+    yi(2) = 0;
+elseif mode==1 % sim recovery from angular velocity perturbation
+    yi(1) = 0;
+    Vpert=Fr.*sqrt(L.*-g);
+    Wpert=Vpert./L; 
+    yi(2)=Wpert;
+end
+%}
+%% Find optimal switch time
+Tswitchi = 0.1; % initial guess at swtich time
+options = optimset('Display','off','TolX',1e-12,'TolFun',1e-12);
+Tswitch = fsolve(@(Tswitch)pendObjective(Tswitch,yi,B,L,M), Tswitchi, options);
+% simulate using optimal switch time
+[t,y] = pendSim(Tswitch,yi,B,L,M);
+% creating torque vector
+indTswitch=find(abs(t-Tswitch)<1e-5);
+Torque_Musc=ones(length(t),1);
+Torque_Musc(1:indTswitch)=B;
+Torque_Musc(indTswitch:end)=-B;
+Torque_Grav=M*-g*L.*sin(y(:,1));
+
+%disp(['Mass:' num2str(M) ' Fall Time:' num2str(t(end))]); 
+% plot results
+if graph==1
+    figure(1); clf; hold on
+    plot(t,radtodeg(y));
+    plot([Tswitch Tswitch],[-50 50],'k.-')
+    grid on
+    xlabel('time (s)');
+    ylabel('Angle (deg) & Angular velocity (deg/sec)')
+    disp('');
+    legend('Angle (deg)','Angular velocity (deg/sec)','Tswitch')
+    
+
+    nam=['Inverted pendulum-' modnam];
+    figure('name',nam)
+    subplot(3,1,1)
+    plot(t,radtodeg(y(:,1)),'r-','LineWidth',3)
+    ylabel('Angle (deg)')
+    grid on;
+%title('5 gram shrew')
+%title('5 ton elephant')
+
+    subplot(3,1,2)
+    plot(t,radtodeg(y(:,2)),'b-','LineWidth',3)
+    ylabel('Angular Velocity (deg/sec)') 
+    grid on;
+
+    subplot(3,1,3)
+    hold on;
+    plot(t,Torque_Musc,'k-','LineWidth',3)
+    plot(t,Torque_Grav,'g-','LineWidth',3)
+    legend('Muscle','Gravity')
+    grid on;
+    ylabel('Torque (Nm)')
+    xlabel('Time (s)')
+
+
+end
+
+% % ID paper graph Posture task kinetics V4.3
+% nam=['Inverted pendulum-' modnam '-' num2str(M)];
+% figure('name',nam)
+% %title('5 gram shrew')
+% title('5 ton elephant')
+% x0=100;
+% y0=100;
+% width=240;
+% height=360;
+% set(gcf,'units','points','position',[x0,y0,width,height])   
+%     
+%     subplot(3,1,1)
+%     plot(t*XX,radtodeg(y(:,1)),'r-','LineWidth',3)
+%     ylabel('Angle (deg)')
+%     axis([min(t)*XX max(t)*XX min(radtodeg(y(:,1)))+0.1*min(radtodeg(y(:,1))) max(radtodeg(y(:,1)))+0.1*max(radtodeg(y(:,1))) ])
+%     
+%     subplot(3,1,2)
+%     plot(t*XX,radtodeg(y(:,2)),'r-','LineWidth',3)
+%     ylabel('Angular Velocity (deg/sec)') 
+%     axis([min(t)*XX max(t)*XX min(radtodeg(y(:,2)))+0.1*min(radtodeg(y(:,2))) max(radtodeg(y(:,2)))+0.1*max(radtodeg(y(:,2))) ])
+% 
+%     
+%     subplot(3,1,3)
+%     hold on;
+%     plot(t*XX,Torque_Musc,'r-','LineWidth',3)
+%     ylabel('Torque (Nm)')
+%     xlabel(['Time ' Tunit])
+%     axis([min(t)*XX max(t)*XX min(Torque_Musc)+0.1*min(Torque_Musc) max(Torque_Musc)+0.1*max(Torque_Musc) ])
+
+if graph==1
+% ID paper graph Posture task kinetics V5
+nam=['Inverted pendulum-' modnam '-' num2str(M)];
+figure('name',nam)
+
+x0=10;
+y0=10;
+width=6;
+height=8;
+set(gcf,'units','centimeters','position',[x0,y0,width,height])
+    
+    subplot(3,1,1)
+    hold on;
+    plot(t*XX,Torque_Musc,'r-','LineWidth',3)
+    %ylabel('Torque (Nm)')
+    axis([min(t)*XX max(t)*XX min(Torque_Musc)+0.1*min(Torque_Musc) max(Torque_Musc)+0.1*max(Torque_Musc) ])
+
+    
+    subplot(3,1,2)
+    plot(t*XX,radtodeg(y(:,2)),'r-','LineWidth',3)
+    %ylabel('Angular Velocity (deg/sec)') 
+    axis([min(t)*XX max(t)*XX min(radtodeg(y(:,2)))+0.1*min(radtodeg(y(:,2))) max(radtodeg(y(:,2)))+0.1*max(radtodeg(y(:,2))) ])  
+       
+    subplot(3,1,3)
+    plot(t*XX,radtodeg(y(:,1)),'r-','LineWidth',3)
+   % ylabel('Angle (deg)')
+    xlabel(['Time ' Tunit])
+    axis([min(t)*XX max(t)*XX min(radtodeg(y(:,1)))+0.1*min(radtodeg(y(:,1))) max(radtodeg(y(:,1)))+0.1*max(radtodeg(y(:,1))) ])
+end
+
+%%
+% if running as script, comment out end below
+end
+
+
+%% In line function definitions
+
+function f = pendObjective(Tswitch,yi,B,L,M)
+% this is the objective function that we are trying to minimize.
+[t,y] = pendSim(Tswitch,yi,B,L,M);
+
+% minimize this objective function. Note that if the event crossing
+% occurred, the second term in this objective function will be nearly zero.
+%f = y(end,1).^2 + y(end,2).^2;
+f = (y(end,1)*1000)^2 + (y(end,2))^2;
+end
+
+function dy = pendEOM(t,y,B,L,M)
+% pendulum equations of motion.
+global g
+dy(1,1) = y(2);
+dy(2,1) = (B+M*9.8066*L*sin(y(1)))/(M*L^2);
+end
+
+function [position,isterminal,direction] = pendEvents(t,y)
+% the event function. We could halt the sim when either angle or velocity
+% is zero, but the optimization works much better when we set the event to
+% zero velocity and the objective function to zero angle.
+position = y(2); % The value that we want to be zero
+isterminal = 1;  % Halt integration 
+direction = 0;   % The zero can be approached from either direction
+end
+
+function [t,y] = pendSim(Tswitch,yi,B,L,M)
+% Once we have an optimal Tswitch, redo the simulation one last time to
+% inspect the results. 
+
+% first simulate from t=0 to t=Tswitch. 
+[t,y] = ode45(@(t,y) pendEOM(t,y,B,L,M), [0 Tswitch], yi);
+
+% use the end state and time as the initial state and time for the new sim
+yinew = y(end,:);
+tspannew = [t(end) 20];
+B = -B; % switch the torque direction.
+
+% this sim stops the sim when the angular velocity is zero, and not at a
+% specific time.
+options = odeset('RelTol',1e-9,'AbsTol',1e-12,'Events',@pendEvents);
+[tnew,ynew,te,ye] = ode45(@(t,y) pendEOM(t,y,B,L,M), tspannew, yinew,options);
+% one interesting thing to note is that if I set the event to be zero
+% angle, the optimization would not converge on zero angular velocity. But
+% when I set the even to zero velocity, and searched for the Tswitch that
+% yielded zero angle, the optimization converged. I think Sayed may also be
+% having this problem. 
+
+% final time and states are the two sim results joined together.
+t = [t(1:end-1);tnew];
+y = [y(1:end-1,:);ynew];
+
+end
+
---
+++ GH_MonteC_InvPendT1.m
@@ -0,0 +1,245 @@
+function [OP]=GH_MonteC_InvPendT1(InitVal2,Niter,Exp,mdl_LlimbF,mdl_LlimbH,PowerL)
+
+%Code to run Monte Carlo sims on the inverted pendulum. Performing Monte
+%Carlos sims only for the Froude perturabtions.
+% Inputs:: 
+% InitVal2 is the Froude number.
+% Niter is the number of iterations.
+% mdl_LlimbF &H are models created using fitlm for the Kilbourne 2013 scaling data for forelimb and hindlimb length. 
+% PowerL is the scaling information for the ankle extensor muscles from Alexander 1981
+% 
+% if a simualation fails, i.e. inverted pendulum falls below 90 degrees, OP.fail outputs a zero for that row. 
+% see IDdoc 5-0-2, 5-0-3 and update 4-1 for more details. 
+    
+graph=0;
+textshow=1;
+global g
+
+%% To run as script, comment out function definition on top and uncomment below. 
+%{
+clear all;close all;clc;
+global g
+graph=1;
+textshow=1;
+g=-9.8066;
+Niter=1e3;% number of Monte carlo sims
+Exp=[-3,log10(0.005),-2,-1,0,1,2,3,log10(5000),4];ind0=find(Exp==0);
+%Inverted pendulum test. Initval is Initial angle. PLa and PLb are results
+% from previous simulations (Master_InvPend.m) without considering the
+% confidence intervals. 
+Testmode='Froude perturbation';
+InitVal2=-0.49;% Initval in Froude number
+PLa=0.080819*1000;% Actual coeff for InitVal from Master_InvPendT1.m
+PLb=0.38568;% Actual expoenent for InitVal 0.49
+
+% scaling data
+load('forelimbCIcalcv3','mdl_Llimb');mdl_LlimbF=mdl_Llimb;clear mdl_Llimb;
+load('hindlimbCIcalcv3','mdl_Llimb');mdl_LlimbH=mdl_Llimb;clear mdl_Llimb;
+
+% Muscle data for hindlimb
+%Alexander, R. M., Jayes, A. S., Maloiy, G. M. O., & Wathuta, E. M. (1981). Allometry of the leg muscles of mammals. Journal of Zoology, 194(4), 539–552. https://doi.org/10.1111/j.1469-7998.1981.tb04600.x
+% Muscle mass and fiber length from Table I: Ankle Extensors, All non hoppers
+% Moment arm data from Table II: Ankle extensors, All non hoppers.
+% Alexander et.al. 1981 reports the 95% confidence intervals on the coefficient "a" and exponent "b".
+%The authors give the the 95% confidence intervals as a */ Range value and b +- Range value. 
+% Here I find the standard error for a and b as: 
+%the whole range of the 95% confidence interval/(2*TcritB) 
+n2=33;%
+TcritB=tinv(1-.025,n2-2);% Critical value for  95% confidence interval from a 33-2 DOF two tailed T distribution. Assuming DOF to be n-2 instead of n-1, since a and b required to perform a linear fit. 
+% Muscle Mass
+PowerL.aMmusc=5.1/1000;% Kg
+PowerL.bMmusc=0.97;% 
+PowerL.aMmuscRange=1.16;
+PowerL.bMmuscRange=0.05;
+%Note: 13th oct 2017,rearranging to match the strcture of coefCI
+PowerL.CIMmusc=[PowerL.aMmusc/PowerL.aMmuscRange PowerL.aMmusc*PowerL.aMmuscRange;PowerL.bMmusc-PowerL.bMmuscRange PowerL.bMmusc+PowerL.bMmuscRange];
+PowerL.bMmuscSE=(PowerL.CIMmusc(2,2)-(PowerL.CIMmusc(2,1)))/(2*TcritB);
+PowerL.aMmuscSE=(PowerL.CIMmusc(1,2)-(PowerL.CIMmusc(1,1)))/(2*TcritB);
+% Fiber length
+PowerL.aFLmusc=10.6/1000;% meters
+PowerL.bFLmusc=0.14;
+PowerL.aFLmuscRange=1.18;
+PowerL.bFLmuscRange=0.08;
+PowerL.CIFLMusc=[PowerL.aFLmusc/PowerL.aFLmuscRange PowerL.aFLmusc*PowerL.aFLmuscRange;PowerL.bFLmusc-PowerL.bFLmuscRange PowerL.bFLmusc+PowerL.bFLmuscRange];
+PowerL.bFLMuscSE=(PowerL.CIFLMusc(2,2)-(PowerL.CIFLMusc(2,1)))/(2*TcritB);
+PowerL.aFLMuscSE=(PowerL.CIFLMusc(1,2)-(PowerL.CIFLMusc(1,1)))/(2*TcritB);
+% Moment arm 
+PowerL.aAMusc=9.4/1000; % meters
+PowerL.bAMusc=0.38;
+PowerL.aAMuscRange=1.07;
+PowerL.bAMuscRange=0.03;
+PowerL.CIAMusc=[PowerL.aAMusc/PowerL.aAMuscRange  PowerL.aAMusc*PowerL.aAMuscRange;PowerL.bAMusc-PowerL.bAMuscRange PowerL.bAMusc+PowerL.bAMuscRange ];
+PowerL.bAMuscSE=(PowerL.CIAMusc(2,2)-(PowerL.CIAMusc(2,1)))/(2*TcritB);
+PowerL.aAMuscSE=(PowerL.CIAMusc(1,2)-(PowerL.CIAMusc(1,1)))/(2*TcritB);
+%}
+%% Predefined matrices
+M=10.^Exp;
+nMusc=33;% Alexander used 33 species to calculate his muscle features scaling. 
+dofMusc=nMusc-2;
+%Initializing matrices
+IPvals.LlimbF=zeros(Niter,length(M));% m
+IPvals.LlimbH=zeros(Niter,length(M));% m
+IPvals.Llimb=zeros(Niter,length(M));% m
+IPvals.MOI=zeros(Niter,length(M));% m
+IPvals.Vpert=zeros(Niter,length(M));
+IPvals.Wpert=zeros(Niter,length(M));
+IPvals.Mmusc=zeros(Niter,length(M));% muscle mass, kg
+IPvals.FLmusc=zeros(Niter,length(M));% muscle fiber length, m
+IPvals.Amusc=zeros(Niter,length(M));% muscle moment arm, m
+IPvals.Tmusc=zeros(Niter,length(M));% muscle torque. Nm
+Coeff.aMmusc=zeros(Niter,1);
+Coeff.bMmusc=zeros(Niter,1);
+Coeff.aFLmusc=zeros(Niter,1);
+Coeff.bFLmusc=zeros(Niter,1);
+Coeff.aAmusc=zeros(Niter,1);
+Coeff.bAmusc=zeros(Niter,1);
+%% Simulation
+
+
+tic
+for nn=1:Niter
+% Inertial properties
+
+IPvals.LlimbF(nn,:)=10.^random(mdl_LlimbF,Exp');
+IPvals.LlimbH(nn,:)=10.^random(mdl_LlimbH,Exp');
+
+IPvals.Llimb(nn,:)=(IPvals.LlimbF(nn,:)+IPvals.LlimbH(nn,:))./2;
+IPvals.MOI(nn,:)=M.*IPvals.Llimb(nn,:).^2;% Point mass moment of inertia 
+
+%Initial Conditions
+    IPvals.Vpert(nn,:)=InitVal2.*sqrt(IPvals.Llimb(nn,:).*-g);
+    IPvals.Wpert(nn,:)=IPvals.Vpert(nn,:)./IPvals.Llimb(nn,:);
+    yinit=[zeros(length(M),1) IPvals.Wpert(nn,:)'];
+
+
+%Muscle properties randomly sampled from a T distribution
+Coeff.aMmusc(nn,1)=PowerL.aMmusc+trnd(dofMusc).*PowerL.aMmuscSE;
+Coeff.bMmusc(nn,1)=PowerL.bMmusc+trnd(dofMusc).*PowerL.bMmuscSE;
+IPvals.Mmusc(nn,:)=Coeff.aMmusc(nn,1)*M.^Coeff.bMmusc(nn,1);% mass of muscle (kg). Triceps All non hopper
+
+Coeff.aFLmusc(nn,1)=PowerL.aFLmusc+trnd(dofMusc).*PowerL.aFLMuscSE;
+Coeff.bFLmusc(nn,1)=PowerL.bFLmusc+trnd(dofMusc).*PowerL.bFLMuscSE;
+IPvals.FLmusc(nn,:)=Coeff.aFLmusc(nn,1)*M.^Coeff.bFLmusc(nn,1);% fiber length muscle (m). Triceps All non hoppers
+rho=1060;%density of muscle in kg/m^3. (Mendeys and keys 1960, Alexander 1977)
+CSmusc=IPvals.Mmusc(nn,:)./rho./IPvals.FLmusc(nn,:).*(100^2);% Cross sectional area of muscle in cm^2
+Fmusc=CSmusc.*20;% assuming 20N/cm^2 
+Coeff.aAmusc(nn,1)=PowerL.aAMusc+trnd(dofMusc).*PowerL.aAMuscSE;
+Coeff.bAmusc(nn,1)=PowerL.bAMusc+trnd(dofMusc).*PowerL.bAMuscSE;
+IPvals.Amusc(nn,:)=Coeff.aAmusc(nn,1)*M.^Coeff.bAmusc(nn,1);% Moment arm value from Triceps, all non hoppers Table II
+IPvals.Tmusc(nn,:)=4*Fmusc.*IPvals.Amusc(nn,:);% Torque by the muscle (Nm)    
+
+%--------------------------------------------------------------------------
+for i=1:length(M)
+       
+    [t,y,Tswitch]=GH_InvPendT1(IPvals.Tmusc(nn,i),IPvals.Llimb(nn,i),M(i),yinit(i,:));
+    
+% If the inverted pendulum has fallen beyond 90 degs, record a bad simulation
+% and enter NaN as movement time. NaNs are ignored by fitlm. 
+    if min(radtodeg(y(:,1)))>-90
+        OP.Tdelay(nn,i)=t(end);
+        OP.goodsim(nn,i)=1;
+    elseif min(radtodeg(y(:,1)))<-90
+        OP.Tdelay(nn,i)=NaN;
+        OP.goodsim(nn,i)=0;
+    end
+    OP.Tswitch(nn,i)=Tswitch;
+    OP.Tdelay2(nn,i)=t(end);%In Tdelay2, I include the bad sims
+    
+    if textshow==1
+    disp(['Simulation number:: ' num2str(nn) ';Mass (kg):: ' num2str(M(i)) ';Torque (Nm):: ' num2str(IPvals.Tmusc(nn,i))]);
+    end
+end
+%--------------------------------------------------------------------------
+%% finding scaling of inertial delay
+% OP.Tdelay, I ignore the bad simulations. 
+mdl=fitlm(log10(M),log10(OP.Tdelay(nn,:)),'linear');% fitlm ignores NaNs 
+OP.b(nn)=mdl.Coefficients.Estimate(2);
+OP.loga(nn)=mdl.Coefficients.Estimate(1);
+
+% in OP.Tdelay, I include the bad simulations
+mdl2=fitlm(log10(M),log10(OP.Tdelay2(nn,:)),'linear');%  
+OP.b2(nn)=mdl2.Coefficients.Estimate(2);
+OP.loga2(nn)=mdl2.Coefficients.Estimate(1);
+
+end
+toc
+
+%% Statistics on a and b of the inertial delay scaling 
+OP.bmean=mean(OP.b);
+OP.bstd=std(OP.b);
+%OP.bSE=OP.bstd/sqrt(Niter);% since running monte carlo sims are like
+%repeatedly sampling the data, and fitting a line is like taking a mean,
+%you dont need to use the formula for estimated standard error "std(x)/sqrt(n)".
+%The standard deviation of the sample mean distribution itself is the standard error
+Tval=tinv([0.025 0.975],Niter-1);
+OP.bCI=OP.bmean+Tval*OP.bstd;
+OP.amean=mean(10.^OP.loga);
+OP.astd=std(10.^OP.loga);
+%OP.aSE=OP.astd/sqrt(Niter);
+OP.aCI=OP.amean+Tval*OP.astd;
+
+
+OP.b2mean=mean(OP.b2);
+OP.b2std=std(OP.b2);
+OP.b2CI=OP.b2mean+Tval*OP.b2std;
+OP.a2mean=mean(10.^OP.loga2);
+OP.a2std=std(10.^OP.loga2);
+%OP.aSE=OP.astd/sqrt(Niter);
+OP.a2CI=OP.a2mean+Tval*OP.a2std;
+
+
+
+Badsimcheck1=sum(OP.goodsim,2);
+Badsimcheck2=Badsimcheck1<length(M);
+OP.badsims=sum(Badsimcheck2);% counts number of sims where muscles could not stop InvPend from falling beyond 90 degs
+%% Saving dataset
+%{
+t=datetime;
+Notes='MonteC_InvPendT1 run of 1000 sims after changing DOF to n-1, ID paper codes, Froude number from 0.01 to 0.49';
+save MonteCsimT1;
+%}
+
+%% Graphing and printing results to screen
+if graph==1
+nam='Variation in Exponent and Coeff';
+figure('name',nam)
+subplot(2,1,1)
+hold on;
+plot(OP.b,'r.-')
+plot(PLb*ones(Niter,1),'k-')
+ylabel('Exponent')
+grid on;
+subplot(2,1,2)
+hold on;
+plot(10.^OP.loga*1000,'g.-')
+plot(PLa*ones(Niter,1),'k-')
+ylabel('Coeff (ms)')
+grid on;
+legend('Monte Carlo values','Actual scaling')
+
+
+nam='Histogram';
+figure('name',nam)
+subplot(2,1,1)
+hold on;
+h1=histogram(OP.b);
+plot([PLb PLb],[0 max(h1.BinCounts)+20],'k-','LineWidth',4)
+grid on;
+xlabel('Exponent')
+ylabel('n')
+subplot(2,1,2)
+hold on;
+h1=histogram(1000*10.^OP.loga);
+plot([PLa PLa],[0 max(h1.BinCounts)+20],'b-','LineWidth',4)
+
+grid on;
+xlabel('Coefficient (ms)')
+ylabel('n')
+
+
+disp(['------------------Final Output-------------'])
+disp(['Scaling of InvPend inertial delay using mean values::' num2str(PLa) '*M^' num2str(PLb) 'ms'  ]);
+disp(['Coefficeint of InvPend inertial delay using Monte Carlo::' num2str(OP.amean*1000) ' +-95%CI [' num2str(OP.aCI*1000) ']'  'ms']);
+disp(['Exponent of InvPend inertial delay using Monte Carlo::' num2str(OP.bmean) ' +-95%CI [' num2str(OP.bCI) ']'  ]);  
+
+end
